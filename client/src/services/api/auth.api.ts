c0786faaf975ec08ef08e950de64aa4992e8253f
Faruq <CodingJay90>
2022-02-22 09:53:56 +0100

    fixes

--- client/src/services/api/auth.api.ts
+++ client/src/services/api/auth.api.ts
@@ -3,7 +3,7 @@ import { getCookie } from "../utils/cookiesFunctions";
 const token = getCookie("auth_token");
 
 const authClient = axios.create({
-  baseURL: "http://localhost:5000/api/v2/auth/",
+  baseURL: "https://wild-traveller-v2.herokuapp.com/api/v2/auth/",
   timeout: 0,
   headers: {
     Accept: "application/vnd.GitHub.v3+json",
